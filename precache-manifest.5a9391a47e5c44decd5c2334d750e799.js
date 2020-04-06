7a5eba59b10d9a7f2afde41fe8e402d0e851ca56
Alex <o.redka@smartgroup.ua>
2020-04-06 17:31:55 +0300

    Updates

---
+++ precache-manifest.5a9391a47e5c44decd5c2334d750e799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264ec3062a5b60520eb6acc2de8d4c77",
+    "url": "/goit-react-hw-02-phonebook-/index.html"
+  },
+  {
+    "revision": "d6a82220dddbe826b54e",
+    "url": "/goit-react-hw-02-phonebook-/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "539ae632f1d9867b6d70",
+    "url": "/goit-react-hw-02-phonebook-/static/css/main.51dd4f67.chunk.css"
+  },
+  {
+    "revision": "d6a82220dddbe826b54e",
+    "url": "/goit-react-hw-02-phonebook-/static/js/2.0f1d5b3b.chunk.js"
+  },
+  {
+    "revision": "539ae632f1d9867b6d70",
+    "url": "/goit-react-hw-02-phonebook-/static/js/main.68957e3c.chunk.js"
+  },
+  {
+    "revision": "e931987eb93a5b0cf5da",
+    "url": "/goit-react-hw-02-phonebook-/static/js/runtime-main.ba57a86a.js"
+  }
+]);
